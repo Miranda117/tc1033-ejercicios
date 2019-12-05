2d83e9f97f14f1c76e76405a7f494080fb824558
Miran117 <a01769232@itesm.mx>
2019-12-04 21:37:27 -0600

    Implementación del perceptrón
    Ahora imprime

--- ejercicio1.py
+++ ejercicio1.py
@@ -11,16 +11,18 @@ def greet(x):
 def perceptron (a,b,c,d):
     resultado=(a*b+c*d)
     if resultado > 20:
-        print("1")
+        print("el numer obtenido es 1")
+        print("Pero el resultado de la operacion es: "resultado)
     else :
-        print ("0")
-
+        print ("el resultado obtenido en la operacion es 0")
+        print("Pero el resultado de la operacion es: "resultado)
+    
 
 
 def run (a):
     for x in range(0,a,1):
         print ("run")
-        
+
 
 if  __name__ == "__main__":
     print("Bienvenido, ¿Cuantas personas usaran este programa?")
