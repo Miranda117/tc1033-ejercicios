dd303f3ac80337c29f20ddf677a3769d1f564e80
Miran117 <a01769232@itesm.mx>
2019-12-04 20:41:46 -0600

    Implementación de la función run

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,14 @@
 def greet(x):
     print("Hola ",x,",¿como estas? ")
 
+
+def run (a):
+    for x in range(0,a,1):
+        print ("run")
+
 if  __name__ == "__main__":
-    print("Bienvenido, cual es su nombre")
+    print("Bienvenido, ¿cual es su nombre?")
     a = input()
     greet(a)
+    b=int(input("ahora difuite un nimero, este debe ser un entero: "))
+    run(b)
